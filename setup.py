3c0dd9626c425b5a94ba3e42aa5d9a68fbb26e18
Vyankat45 <https://github.com/Vyankat45>
2026-07-23 11:17:49 +0530

    Project Structure Is Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vyankat Rathod",
+    author_email="imvyankat45@gmail.com",
+    packages=find_packages()
+)
