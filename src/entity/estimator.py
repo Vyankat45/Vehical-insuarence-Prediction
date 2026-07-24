12458935af2a745eb0084a1fbbae03027e3d41e4
Vyankat45 <https://github.com/Vyankat45>
2026-07-24 12:49:14 +0530

    AWS servises Done and And model has been pushed on s3 bucket

--- src/entity/estimator.py
+++ src/entity/estimator.py
@@ -1,42 +1,55 @@
-from dataclasses import dataclass
+import sys
+
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+
+from src.exception import MyException
+from src.logger import logging
+
+class TargetValueMapping:
+    def __init__(self):
+        self.yes:int = 0
+        self.no:int = 1
+    def _asdict(self):
+        return self.__dict__
+    def reverse_mapping(self):
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
+
+class MyModel:
+    def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
+        """
+        :param preprocessing_object: Input Object of preprocesser
+        :param trained_model_object: Input Object of trained model 
+        """
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+
+    def predict(self, dataframe: pd.DataFrame) -> DataFrame:
+        """
+        Function accepts preprocessed inputs (with all custom transformations already applied),
+        applies scaling using preprocessing_object, and performs prediction on transformed features.
+        """
+        try:
+            logging.info("Starting prediction process.")
+
+            # Step 1: Apply scaling transformations using the pre-trained preprocessing object
+            transformed_feature = self.preprocessing_object.transform(dataframe)
+
+            # Step 2: Perform prediction using the trained model
+            logging.info("Using the trained model to get predictions")
+            predictions = self.trained_model_object.predict(transformed_feature)
+
+            return predictions
+
+        except Exception as e:
+            logging.error("Error occurred in predict method", exc_info=True)
+            raise MyException(e, sys) from e
 
 
-@dataclass
-class DataIngestionArtifact:
-    trained_file_path:str 
-    test_file_path:str
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
 
-@dataclass
-class DataValidationArtifact:
-    validation_status:bool
-    message: str
-    validation_report_file_path: str
-
-@dataclass
-class DataTransformationArtifact:
-    transformed_object_file_path:str 
-    transformed_train_file_path:str
-    transformed_test_file_path:str
-
-@dataclass
-class ClassificationMetricArtifact:
-    f1_score:float
-    precision_score:float
-    recall_score:float
-
-@dataclass
-class ModelTrainerArtifact:
-    trained_model_file_path:str 
-    metric_artifact:ClassificationMetricArtifact
-
-@dataclass
-class ModelEvaluationArtifact:
-    is_model_accepted:bool
-    changed_accuracy:float
-    s3_model_path:str 
-    trained_model_path:str
-
-@dataclass
-class ModelPusherArtifact:
-    bucket_name:str
-    s3_model_path:str
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
